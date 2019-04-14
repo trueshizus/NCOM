333f343bbc5f24cd7e1f8d8a9e583953adf7d06a
Shizus <JesusHerrera@users.noreply.github.com>
2019-04-14 03:17:17 -0300

    basic screen created

---
+++ src/components/NcomCalculator/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NcomCalculator() {
+  return (
+    <div>
+      <input type="text" />
+      <span>=0</span>
+    </div>
+  );
+}
+
+export default NcomCalculator;
